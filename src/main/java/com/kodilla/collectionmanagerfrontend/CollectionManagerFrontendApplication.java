3d201f8129ca205d6605a75989cf756ef39a60cb
J-Fi <y.neczek@gmail.com>
2019-12-22 03:00:07 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/collectionmanagerfrontend/CollectionManagerFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.collectionmanagerfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionManagerFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionManagerFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/collectionmanagerfrontend/CollectionManagerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.collectionmanagerfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionManagerFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
